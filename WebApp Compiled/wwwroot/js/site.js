fde06bc72ea5fa921737c02bc899e69f8bb2034b
MorleyT946 <74619974+MorleyT946@users.noreply.github.com>
2020-11-17 18:44:51 +0000

    Add files via upload

---
+++ WebApp Compiled/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿
+$(document).ready(function () {
+    $("#frmAlarmMessage").validate({
+        rules: {
+            Message: {
+                required: true,
+                minlength: 5
+            }
+        },
+        submitHandler: function (form) {
+            form.submit();
+        }
+    });
+});
